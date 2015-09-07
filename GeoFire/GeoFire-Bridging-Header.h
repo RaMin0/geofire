c2d1705691f9d7754bff9e69bf8fa0ebcf31e9d3
Antoni Espinosa <antoniespinosa@me.com>
2015-09-07 16:05:31 +0200

    Bridging Header

---
+++ GeoFire/GeoFire-Bridging-Header.h
@@ -0,0 +1,14 @@
+// Copyright (C) 2015 Meetizer S.L. - All Rights Reserved
+//
+// Unauthorized copying of this file, via any medium is strictly prohibited
+// Proprietary and confidential.
+
+#import "GeoFire+Private.h"
+#import "GFQuery+Private.h"
+#import "GFBase32Utils.h"
+#import "GFGeoHash.h"
+#import "GFGeoHashQuery.h"
+#import "GeoFire.h"
+#import "GFQuery.h"
+#import "GFCircleQuery.h"
+#import "GFRegionQuery.h"
