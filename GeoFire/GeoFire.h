abdcbc6264da72de966953374b884e0eed48ed9a
Antoni Espinosa <antoniespinosa@me.com>
2015-09-07 13:29:04 +0200

    Initial commit

---
+++ GeoFire/GeoFire.h
@@ -0,0 +1,19 @@
+//
+//  GeoFire.h
+//  GeoFire
+//
+//  Created by Antoni Espinosa  on 7/9/15.
+//  Copyright © 2015 Meetizer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GeoFire.
+FOUNDATION_EXPORT double GeoFireVersionNumber;
+
+//! Project version string for GeoFire.
+FOUNDATION_EXPORT const unsigned char GeoFireVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GeoFire/PublicHeader.h>
+
+
